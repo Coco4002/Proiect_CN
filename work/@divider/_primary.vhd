b0be3a88600e170127fba1b5e1858b2955c528b1
Coco <cocodarius04@yahoo.com>
2025-05-08 21:21:19 +0300

    Final 2

--- work/@divider/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Divider is
-    port(
-        Dividend        : in     vl_logic_vector(7 downto 0);
-        Divisor         : in     vl_logic_vector(7 downto 0);
-        Quotient        : out    vl_logic_vector(7 downto 0)
-    );
-end Divider;
